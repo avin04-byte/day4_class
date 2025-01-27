bc35c4e1e47e9807163529b07a13ac969a2a09ca
avin04-byte <2311cs020032@mallareddyuniversity.ac.in>
2025-01-27 19:52:18 +0530

    Add files via upload

---
+++ exler assignment2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+my_dict = {
+    'name': 'Avin',
+    'age': 20,
+    'city': 'Hydearbad',
+    'country': 'India',
+    'profession': 'Engineer'
+}
+
+
+print(my_dict['name'])       
+print(my_dict['city'])       
+
+
+print(my_dict.get('age'))    
+print(my_dict.get('salary', 'Not available'))  
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
